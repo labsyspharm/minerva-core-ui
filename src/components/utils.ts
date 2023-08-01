df6de2703cc5d8a746698f984eb46e6208649060
Simon Warchol <simonwarchol@g.harvard.edu>
2023-08-01 15:24:50 -0400

    more stuff

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,10 +1,6 @@
-import { OrthographicView } from '@deck.gl/core';
 import { Matrix4 } from '@math.gl/core';
 import { MAX_CHANNELS, DTYPE_VALUES } from '@vivjs/constants';
 
-export function range(len) {
-  return [...Array(len).keys()];
-}
 
 /**
  * @template T
@@ -126,23 +122,3 @@ export function getPhysicalSizeScalingMatrix(loader) {
   return new Matrix4().identity();
 }
 
-/**
- * Create a boudning box from a viewport based on passed-in viewState.
- * @param {Object} viewState The viewState for a certain viewport.
- * @returns {View} The DeckGL View for this viewport.
- */
-export function makeBoundingBox(viewState) {
-  const viewport = new OrthographicView().makeViewport({
-    // From the current `detail` viewState, we need its projection matrix (actually the inverse).
-    viewState,
-    height: viewState.height,
-    width: viewState.width
-  });
-  // Use the inverse of the projection matrix to map screen to the view space.
-  return [
-    viewport.unproject([0, 0]),
-    viewport.unproject([viewport.width, 0]),
-    viewport.unproject([viewport.width, viewport.height]),
-    viewport.unproject([0, viewport.height])
-  ];
-}
