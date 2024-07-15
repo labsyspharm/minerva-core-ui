2e43271f168c4d6d2795eebad94abcc1397f5676
John Hoffer <john@hoff.in>
2024-07-15 17:15:47 -0400

    update deps

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   server: { https: true },
-  plugins: [ mkcert() ],
+  plugins: [
+    mkcert(), 
+  ],
   build: {
     sourcemap: true
   }
