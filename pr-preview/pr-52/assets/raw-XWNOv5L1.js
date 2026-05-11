16ecd0f7c6c114a881ec0a66d1c640bdc030a37f
simonwarchol <simonwarchol@users.noreply.github.com>
2026-05-11 16:47:13 +0000

    Deploy preview for PR 52 🛫

---
+++ pr-preview/pr-52/assets/raw-XWNOv5L1.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./decoder.worker-DWqYPgiQ.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
