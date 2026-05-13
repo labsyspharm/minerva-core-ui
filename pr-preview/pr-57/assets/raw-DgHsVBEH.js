5c72d14ea5ae7ac907589b3867c6d6d1fb88971b
simonwarchol <simonwarchol@users.noreply.github.com>
2026-05-13 13:21:27 +0000

    Remove preview for PR 57 🛬

--- pr-preview/pr-57/assets/raw-DgHsVBEH.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-DdrJZEfV.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
