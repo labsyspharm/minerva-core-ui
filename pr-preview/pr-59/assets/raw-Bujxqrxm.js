aa880c4d13ce7964b7a6e0ea3a704ea031add60d
simonwarchol <simonwarchol@users.noreply.github.com>
2026-05-14 19:42:00 +0000

    Deploy preview for PR 59 🛫

---
+++ pr-preview/pr-59/assets/raw-Bujxqrxm.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-DiG41zHE.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
