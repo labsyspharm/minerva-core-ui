e60700cf551f5baa15b1c42c5f08a6954351705e
simonwarchol <simonwarchol@users.noreply.github.com>
2026-06-08 15:10:26 +0000

    Deploy preview for PR 60 🛫

--- pr-preview/pr-60/assets/raw-BNjYeql6.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-B8-k097r.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
