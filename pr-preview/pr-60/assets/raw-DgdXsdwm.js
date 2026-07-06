c847eb9a5342ec987971d91c5a679c1b6b05f808
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-06 16:35:16 +0000

    Deploy preview for PR 60 🛫

---
+++ pr-preview/pr-60/assets/raw-DgdXsdwm.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-BnlB1FYc.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
