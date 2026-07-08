1910ee0ae6ca9e2836ba40c5556763b16ca5bd8d
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-08 19:11:01 +0000

    Remove preview for PR 61 🛬

--- pr-preview/pr-61/assets/raw-Bszs_k37.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-Dfen6zmb.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
