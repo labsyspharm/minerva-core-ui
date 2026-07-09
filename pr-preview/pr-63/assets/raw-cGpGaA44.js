d450b58a32fd912efe3b775a0c50a2db766444f9
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-09 15:49:53 +0000

    Remove preview for PR 63 🛬

--- pr-preview/pr-63/assets/raw-cGpGaA44.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-CqJ1AjyU.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
