4f8cefa0a30df69f40e7b1225fdc11e6fe6e5269
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-09 15:34:45 +0000

    Deploy preview for PR 63 🛫

---
+++ pr-preview/pr-63/assets/raw-cGpGaA44.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-CqJ1AjyU.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
