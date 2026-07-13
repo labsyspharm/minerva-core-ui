3b1f65eb7be474bdcec92b2ea4ad5c598d496c2a
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-13 18:58:48 +0000

    Deploy preview for PR 65 🛫

---
+++ pr-preview/pr-65/assets/raw-BS6UR-j3.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-B2cMAZPa.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
