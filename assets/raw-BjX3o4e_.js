09695bfacad8a3d43164a4beede1a4f4fc4fdf42
simonwarchol <me@simonwarchol.com>
2026-07-17 20:50:56 +0000

    Deploying to gh-pages from @ labsyspharm/minerva-core-ui@a8235b0834d28d56b4cd015c15672c84fc1d8c62 🚀

---
+++ assets/raw-BjX3o4e_.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-Dvb8Uw75.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
