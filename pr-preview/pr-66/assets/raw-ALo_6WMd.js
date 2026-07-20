ef8d930619ac80c3d4f55bc525889ccb0a08463e
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-20 18:12:48 +0000

    Deploy preview for PR 66 🛫

--- pr-preview/pr-66/assets/raw-ALo_6WMd.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-Dm4fWRDz.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
