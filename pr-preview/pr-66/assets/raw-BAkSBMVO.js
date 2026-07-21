c734cb9f255d3e5a8c996ab1904b9a3fb139988c
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-21 15:54:43 +0000

    Deploy preview for PR 66 🛫

---
+++ pr-preview/pr-66/assets/raw-BAkSBMVO.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-B3evIP9S.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
