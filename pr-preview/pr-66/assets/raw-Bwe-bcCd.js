3d591baa835d706b8a5e7942f66759c4a520bca6
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-21 15:19:08 +0000

    Deploy preview for PR 66 🛫

--- pr-preview/pr-66/assets/raw-Bwe-bcCd.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-1yg7D3na.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
