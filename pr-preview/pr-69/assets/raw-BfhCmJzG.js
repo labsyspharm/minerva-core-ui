96212ae970d141070008dd92c77104b8a735abb7
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-22 21:49:53 +0000

    Remove preview for PR 69 🛬

--- pr-preview/pr-69/assets/raw-BfhCmJzG.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-DuX1qzjV.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
