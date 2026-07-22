4987618e26dedced5a05487697a7fb2619de67d0
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-22 20:48:01 +0000

    Deploy preview for PR 69 🛫

---
+++ pr-preview/pr-69/assets/raw-CYCzXUAb.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-Cu69feoz.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
