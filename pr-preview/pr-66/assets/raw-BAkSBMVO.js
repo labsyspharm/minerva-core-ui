7f07efc7786a6b70b09afec81498c2ce952f0f94
simonwarchol <simonwarchol@users.noreply.github.com>
2026-07-22 19:31:41 +0000

    Remove preview for PR 66 🛬

--- pr-preview/pr-66/assets/raw-BAkSBMVO.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-B3evIP9S.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
