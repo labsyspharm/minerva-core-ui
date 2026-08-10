119cea57b5ad26fe2648fbc98e5fbc5ca5c16a78
simonwarchol <simonwarchol@users.noreply.github.com>
2026-08-10 19:19:27 +0000

    Remove preview for PR 73 🛬

--- pr-preview/pr-73/assets/raw-VtUWjSxU.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-cd2ncwP3.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
