37c3c94ff82f198eb1a32255b7d2f9852939bd1a
simonwarchol <simonwarchol@users.noreply.github.com>
2026-08-11 16:28:28 +0000

    Deploy preview for PR 74 🛫

---
+++ pr-preview/pr-74/assets/raw-DKwptc23.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-BjdTG1zg.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
