56500189e608cbfa468208ae90a2c82078169b8a
simonwarchol <simonwarchol@users.noreply.github.com>
2026-08-13 13:49:09 +0000

    Remove preview for PR 74 🛬

--- pr-preview/pr-74/assets/raw-DKwptc23.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-BjdTG1zg.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
