ce1c51e9fa4dbcec971e488cda7f8c4be326f85f
simonwarchol <simonwarchol@users.noreply.github.com>
2026-09-16 19:44:12 +0000

    Remove preview for PR 104 🛬

--- pr-preview/pr-104/assets/raw-BVl6BAiY.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-C-ORxWLY.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
