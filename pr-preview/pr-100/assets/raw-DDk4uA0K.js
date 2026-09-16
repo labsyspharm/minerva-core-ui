d4f8a6c9eb26d351ad70bb3f5ad56db8f3e8fc5f
simonwarchol <simonwarchol@users.noreply.github.com>
2026-09-16 19:44:53 +0000

    Remove preview for PR 100 🛬

--- pr-preview/pr-100/assets/raw-DDk4uA0K.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from './decoder.worker-BB8BIQv9.js';
-
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-
-export { RawDecoder as default };
