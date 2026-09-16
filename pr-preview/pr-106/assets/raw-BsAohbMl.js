c0c8fa44e29a351e1c6878f0eb35d307a14a38a6
thejohnhoffer <thejohnhoffer@users.noreply.github.com>
2026-09-16 17:22:52 +0000

    Deploy preview for PR 106 🛫

---
+++ pr-preview/pr-106/assets/raw-BsAohbMl.js
@@ -0,0 +1,9 @@
+import { B as BaseDecoder } from "./index-CuSg_R6j.js";
+class RawDecoder extends BaseDecoder {
+  decodeBlock(buffer) {
+    return buffer;
+  }
+}
+export {
+  RawDecoder as default
+};
