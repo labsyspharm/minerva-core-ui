5bdd7ea3ec66bb1fce600026741119c8ff47ac86
simonwarchol <simonwarchol@users.noreply.github.com>
2026-09-21 19:37:10 +0000

    Remove preview for PR 109 🛬

--- pr-preview/pr-109/assets/raw-D_OtMG7h.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-BUT6RXB1.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
